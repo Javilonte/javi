adba0c09d5965799d35020ce28fd8f793b2cf235
javilonte <javgofi@gmail.com>
2023-05-10 01:04:39 -0600

    updated image source

--- javi-personal-web/src/components/About/About.jsx
+++ javi-personal-web/src/components/About/About.jsx
@@ -1,13 +1,17 @@
+import './about.css'
+
 function About() {
   return (
     <>
     
     <div className="about-container">
-        <div className="about-content">
-            <h1 className='about-title'>Sobre mi</h1>
-            <p className='about-text'>Soy un desarrollador web que busca aprender y mejorar cada día. Me gusta trabajar en equipo y compartir conocimientos. Me considero una persona responsable, creativa y con muchas ganas de aprender.</p>
-  
-        </div>        
+        <div className="about-content-up">
+            <h1 className='about-title'>About me</h1>
+            <p className='about-text'>I am a 27-year-old self-taught programmer with a strong passion for new technologies. I stand out for my adaptability to change and my ability to give and take feedback. With extensive experience in software development, I am looking for a new challenge that allows me to continue growing and developing my skills.</p>
+        </div>
+        <div className="about-content-down">
+        <img src="https://scontent.fgdl2-2.fna.fbcdn.net/v/t39.30808-6/345044922_659020946063524_2036168192479899498_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=ENiFu4DijrMAX_-emft&_nc_ht=scontent.fgdl2-2.fna&oh=00_AfDhsq4I_G1mTWSyozDCeZ1BOebgN1VOTw9jIvnnYibUfQ&oe=645F7467"></img>
+        </div>       
     </div>
     
     </>
